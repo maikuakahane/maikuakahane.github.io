949210c260dade8ba6f26b07e0a487160bfb0327
maikuakahane <126707057+maikuakahane@users.noreply.github.com>
2023-07-14 13:46:34 +0100

    Update NavDivs.js

--- NavDivs.js
+++ NavDivs.js
@@ -1,1 +1,18 @@
 // JavaScript source code
+export var BCQFNAV02 = `
+<div class="questform" id="bcqfnav02">
+  <h2> Whose certificate you are requesting?</h2>
+  <form>
+    <div>
+    <input type="radio" id="mechoice1" name="forme" checked />
+    <label for="mechoice1"> For myself </label>
+  </div>
+  <div></div>
+    <input type="radio" id="mechoice2" name="forme" />
+    <label for="mechoice2"> For someone else </label>
+    </div>
+  <div class="button">
+                <input type="button" id="nav02btn" value="Potvrdit" />
+            </div>
+  </form>
+</div> `
