d0d7a80379aaf221020933d3c6260af30f0609c9
maikuakahane <126707057+maikuakahane@users.noreply.github.com>
2023-07-14 13:21:02 +0100

    Update Mainscript.js

--- Mainscript.js
+++ Mainscript.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+
+
+
+
+window.onload => () {
+  let BCBTN = document.getElementById("bcbtn_en")
+  let MCBTN = document.getElementById("mcbtn_en")
+  let DCBTN = document.getElementById("dcbtn_en")
+  var NAVF = document.getElementById("CCqfnav01")
+
+  BCBTN.onclick => () {
+    NAVF.innerHTML = 
+  }
+}
