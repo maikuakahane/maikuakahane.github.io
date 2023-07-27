cb4f4df6304009ce69e74ed9008b306d14226246
maikuakahane <126707057+maikuakahane@users.noreply.github.com>
2023-07-27 16:04:58 +0100

    Update PrintDivs.js

--- PrintDivs.js
+++ PrintDivs.js
@@ -10,60 +10,62 @@ Questionnaire for application to send a registry certificate from Czech Republic
 <table>
 <tr>
 <td> Současné příjmení/ Current surname </td>
-<td>  </td>
+<td id="PCsname01">  </td>
 </tr>
 <tr>
 <td> Rodné příjmení/surname at birth </td>
-<td> </td>
+<td id="PBsname01"> </td>
 </tr>
 <tr>
 <td> Jméno/first and middle names </td>
-<td> </td>
+<td id="PFname01"> </td>
 </tr>
 <tr>
 <td> Datum narození/date of birth </td>
-<td> </td>
+<td id="PDob01"> </td>
 </tr>
 <tr>
-<td> Místo a  okres narození/place and district of birth </td>
-<td> </td>
-</tr>
+<td> Místo narození/place of birth </td>
+<td id="PPob01"> </td>
 </tr>
 <tr>
-<td> náboženství v době narození u osob, které se narodily do 31. 12. 1949/ religion at time of birth prior to 31.12.1949 </td>
-<td> </td>
+<td> Stát narození narození/country of birth </td>
+<td id="PPob01"> </td>
 </tr>
+<tr>
+<td> Náboženství v době narození u osob, které se narodily do 31. 12. 1949/ religion at time of birth prior to 31.12.1949 </td>
+<td id="PReli"> </td>
 </tr>
 <tr>
 <td> Nynější bydliště/current address </td>
-<td> </td>
+<td id="PPadd01"> </td>
 </tr>
 </table>
 <h4> Údaje  rodičích osoby uvedené v bodu B/details of parents of person listed in point B </h4>
 <table>
 <tr>
 <td> Současné příjmení jejich otce/ Current surname </td>
-<td>  </td>
+<td id="PCsname04">  </td>
 </tr>
 <tr>
 <td> Rodné příjmení jejich otce/surname at birth </td>
-<td> </td>
+<td id="PBsname04"> </td>
 </tr>
 <tr>
 <td> Jméno jejich otce/ first and middle names </td>
-<td> </td>
+<td id="PFname04"> </td>
 </tr>
 <tr>
 <td> Současné příjmení jejich matky/ Current surname </td>
-<td>  </td>
+<td id="PCsname03">  </td>
 </tr>
 <tr>
 <td> Rodné příjmení jejich matky/surname at birth </td>
-<td> </td>
+<td id="PBsname03"> </td>
 </tr>
 <tr>
 <td> Jméno jejich matky/ first and middle names </td>
-<td> </td>
+<td id="PFname03"> </td>
 </tr>
 </table>
 </div>
@@ -72,15 +74,120 @@ Questionnaire for application to send a registry certificate from Czech Republic
 <table>
 <tr>
 <td> Jméno/first and middle names </td>
-<td> </td>
+<td id="PFname05"> </td>
 </tr>
 <tr>
 <td> Současné příjmení/ Current surname </td>
-<td>  </td>
+<td id="PCsname05">  </td>
 </tr>
 <tr>
 <td> Rodinný poměr k osobě, na jejíž jméno má být doklad vystaven nebo uvedení právního zájmu na vystavení dokladu/family relationship to the person whose certificate should be issued or statement of legal interest on issue of the certificate </td>
-<td>  </td>
+<td id="PRela">  </td>
+</tr>
+</table>
+<br>
+V/In (.......................)dne/date (.............................) podpis žadatele/signature of applicant (...........................)
+<br>
+-------------------------------------------------------------------------------------------------------------------------------------------
+<br>
+Totožnost žadatele ověřena podle dokladu .....................................................................
+<br> vydaného dne ......./......./.......   kým ..................... platného do ......./......./.......
+<br>
+Totožnost žadatele ověřil:
+<br>
+....................................................................................
+<br>
+jméno, příjmení, funkce a podpis konzulárního úředníka 
+<br>
+....................................................................................
+<br>
+razítko zastupitelského úřadu ČR 
+</div> `
+
+export var BCDPRINTB =  `
+<div class="printform" id="bcqprint01">
+<h3> Dotazník k žádosti o zaslání matričního dokladu z ČR
+<br>
+Questionnaire for application to send a registry certificate from Czech Republic</h3>
+<h4> Označení požadovaného dokladu/Designation of requested certicfcate </h4>
+<b>Rodný list/birth certificace </b>
+<h4> Údaje o osobě, které se žádaný doklad týká/ Details of the person on the certificate </h4>
+<table>
+<tr>
+<td> Současné příjmení/ Current surname </td>
+<td id="PCsname01">  </td>
+</tr>
+<tr>
+<td> Rodné příjmení/surname at birth </td>
+<td id="PBsname01"> </td>
+</tr>
+<tr>
+<td> Jméno/first and middle names </td>
+<td id="PFname01"> </td>
+</tr>
+<tr>
+<td> Datum narození/date of birth </td>
+<td id="PDob01"> </td>
+</tr>
+<tr>
+<td> Místo narození/place of birth </td>
+<td id="PPob01"> </td>
+</tr>
+<tr>
+<td> Stát narození narození/country of birth </td>
+<td id="PPob01"> </td>
+</tr>
+<tr>
+<td> Náboženství v době narození u osob, které se narodily do 31. 12. 1949/ religion at time of birth prior to 31.12.1949 </td>
+<td id="PReli"> </td>
+</tr>
+</table>
+<h4> Údaje  rodičích osoby uvedené v bodu B/details of parents of person listed in point B </h4>
+<table>
+<tr>
+<td> Současné příjmení jejich otce/ Current surname </td>
+<td id="PCsname04">  </td>
+</tr>
+<tr>
+<td> Rodné příjmení jejich otce/surname at birth </td>
+<td id="PBsname04"> </td>
+</tr>
+<tr>
+<td> Jméno jejich otce/ first and middle names </td>
+<td id="PFname04"> </td>
+</tr>
+<tr>
+<td> Současné příjmení jejich matky/ Current surname </td>
+<td id="PCsname03">  </td>
+</tr>
+<tr>
+<td> Rodné příjmení jejich matky/surname at birth </td>
+<td id="PBsname03"> </td>
+</tr>
+<tr>
+<td> Jméno jejich matky/ first and middle names </td>
+<td id="PFname03"> </td>
+</tr>
+</table>
+</div>
+<div class="printform" id="bcqprint02">
+<h4> Údaje o osobě, která žádá o vystavení dokladu, není-li totožná s osobou v bodě B/details of the person requesting issue of the certificate if not the same as person listed in point B </h4>
+<table>
+<tr>
+<td> Jméno/first and middle names </td>
+<td id="PFname05"> </td>
+</tr>
+<tr>
+<td> Současné příjmení/ Current surname </td>
+<td id="PCsname05">  </td>
+</tr>
+<tr>
+<td> Rodinný poměr k osobě, na jejíž jméno má být doklad vystaven nebo uvedení právního zájmu na vystavení dokladu/family relationship to the person whose certificate should be issued or statement of legal interest on issue of the certificate </td>
+<td id="PRela">  </td>
+</tr>
+<tr>
+<td> Nynější bydliště/current address </td>
+<td id="PPadd01"> </td>
 </tr>
 </table>
 <br>
